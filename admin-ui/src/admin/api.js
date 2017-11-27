5c37af0e56b1176afdf070226cc0ef460044c061
Juha Syrjälä <juha.syrjala@iki.fi>
2017-11-27 14:01:53 -0800

    Placeholders for rejectAlert and approveAlert

--- admin-ui/src/admin/api.js
+++ admin-ui/src/admin/api.js
@@ -1,10 +1,17 @@
+/**
+ * AWS = this.props.AWS in React
+ */
 export const listPendingAlerts = (AWS) => {
   let docClient = new AWS.DynamoDB.DocumentClient();
   const params = {
     TableName: window.config.aws.dynamoPendingAlerts,
+    KeyConditionExpression: "#s = 'pending'",
+    ExpressionAttributeNames: {
+      "#s": "status"
+    },
   }
   return new Promise((resolve, reject) => {
-    docClient.scan(params, (err, data) => {
+    docClient.query(params, (err, data) => {
       if(err) {
         console.error('Failed to fetch pending alerts', err)
         reject(err)
@@ -14,3 +21,10 @@ export const listPendingAlerts = (AWS) => {
   })
 }
 
+export const approveAlert = (AWS, alert) => {
+  console.error('approveAlert is not implemented yet')
+}
+
+export const rejectAlert = (AWS, alert) => {
+  console.error('rejectAlert is not implemented yet')
+}

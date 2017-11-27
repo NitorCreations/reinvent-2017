164b1d46b390ec53394a4ddce6108faa03c36073
Richard Weber <richard.weber@nitor.com>
2017-11-27 23:01:51 +0200

    Working alert creation API

--- serverless/inputApi.js
+++ serverless/inputApi.js
@@ -1,20 +1,48 @@
 "use strict";
 
 
-const getResponse = () => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!'
-        })
-    };
-};
-
+const AWS = require('aws-sdk');
+const uuidv1 = require('uuid/v1');
 
 module.exports.createAlert = (event, context, callback) => {
-    callback(null, getResponse());
+    console.log(event);
+
+    const requestBody = event.body;
+    const alertId = uuidv1();
+    const db = new AWS.DynamoDB();
+
+    let alert = JSON.parse(requestBody);
+
+    let item = {
+        id: {S: alertId},
+        type: {S: alert.type},
+        description: {S: alert.description},
+        time: {S: alert.time},
+        lon: {S: alert.lon},
+        lat: {S: alert.lat}
+    };
+    console.log(item);
+
+    db.putItem({
+        TableName: process.env.NOT_APPROVED_ALERTS_TABLE,
+        Item: item
+    }, (err, data) => {
+        if(err) {
+            console.log(err);
+            callback(null, {
+                statusCode: 500,
+                body: JSON.stringify({status: "ERROR", err: err})
+            });
+        } else {
+            callback(null, {
+                statusCode: 201,
+                body: JSON.stringify({status: "OK", alertId: alertId})
+            });
+        }
+    });
 };
 
+
 module.exports.linkImage = (event, context, callback) => {
-    callback(null, getResponse());
+    callback(null, getResponse(event));
 };

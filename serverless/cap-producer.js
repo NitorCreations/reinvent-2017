83dd4202d9f6c3158adef3536f774e8bda50b3df
Timo Tenhunen <timo.tenhunen@nitor.fi>
2017-11-27 11:41:41 -0800

    Simple skeleton for producing cap

---
+++ serverless/cap-producer.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const json2xml = require('json2xml');
+const _ = require('lodash');
+
+//module.exports.produceCap = (event, context, callback) => {
+	function produceCap () {
+
+		const testAlertJson = {
+			"alert": {
+				"type": "security",
+				"location": {
+					"lon": 25.6,
+					"lat": 60.0
+				},
+				"description": [
+					{
+						language: "en-us",
+						text : "this is sparta"
+					}, 
+					{
+						language : "es-US",
+						text : "this is spanish sparta"
+					}
+				],
+				"time": "2017-11-26T10:34:56.123Z"
+			}
+		}
+		console.log("yaya");
+
+		//TODO implement parsing of localized alert infos, now only taking the first one
+		/*
+		const capInfo = _.map(testAlertJson.alert.description, (localInfo) => {
+			console.log("1:", localInfo)
+			return  {
+				language : localInfo.language,
+				description : localInfo.text
+			}
+		});
+		*/
+		const capInfo = (testAlertJson.alert.description) ? testAlertJson.alert.description[0] : null;
+		console.log("capInfo", capInfo);
+
+		const capXml = {
+			alert : {
+				identifier: testAlertJson.alert.identifier,
+				sender: testAlertJson.alert.sender,
+				sent: testAlertJson.alert.sent,
+				status: testAlertJson.alert.actual,
+				msgType: testAlertJson.alert.msgType,
+				scope: testAlertJson.alert.scope,
+				info: capInfo
+			},
+			attr : { namespace : 'lol'}
+
+		};
+		console.log(json2xml(capXml, { header: true, attributes_key: 'attr' }));
+
+	};
+
+produceCap();
+
+/*
+<identifier>TRI13970876.2</identifier> 
+  <sender>trinet@caltech.edu</sender> 
+  <sent>2003-06-11T20:56:00-07:00</sent>
+  <status>Actual</status> 
+  <msgType>Update</msgType>
+  <scope>Public</scope>
+  <references>trinet@caltech.edu,TRI13970876.1,2003-06-11T20:30:00-07:00</references>
+  <info>
+    <category>Geo</category>
+    <event>Earthquake</event>   
+    <urgency>Past</urgency>   
+    <severity>Minor</severity>   
+    <certainty>Observed</certainty>
+    <senderName>Southern California Seismic Network (TriNet) operated by Caltech and USGS</senderName>
+    <headline>EQ 3.4 Imperial County CA</headline>
+    <description>A minor earthquake measuring 3.4 on the Richter scale occurred near Brawley, California at 8:30 PM Pacific Daylight Time on Wednesday, June 11, 2003. (This event has now been reviewed by a seismologist)</description>
+    <web>http://www.trinet.org/scsn/scsn.html</web>
+    <parameter>
+      <valueName>EventID</valueName>
+      <value>13970876</value>
+    </parameter>
+    <parameter>
+      <valueName>Version</valueName>
+      <value>1</value>
+    </parameter>
+    <parameter>
+      <valueName>Magnitude</valueName>
+      <value>3.4 Ml</value>
+    </parameter>
+    <parameter>
+      <valueName>Depth</valueName>
+      <value>11.8 mi.</value>
+    </parameter>
+    <parameter>
+      <valueName>Quality</valueName>
+      <value>Excellent</value>
+    </parameter>
+    <area>       
+      <areaDesc>1 mi. WSW of Brawley, CA; 11 mi. N of El Centro, CA; 30 mi. E of OCOTILLO (quarry); 1 mi. N of the Imperial Fault</areaDesc>
+      <circle>32.9525,-115.5527 0</circle>  
+    </area>
+  </info>*/

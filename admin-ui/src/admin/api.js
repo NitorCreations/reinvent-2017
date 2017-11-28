82cbc33a410a577d11f6c5f1fb1e4dcfb29c3a54
Juha Syrjälä <juha.syrjala@iki.fi>
2017-11-27 16:08:22 -0800

    Fetch data from dynamodb

--- admin-ui/src/admin/api.js
+++ admin-ui/src/admin/api.js
@@ -1,40 +1,33 @@
+import _ from 'lodash'
+
 /**
  * AWS = this.props.AWS in React
  */
 export const listPendingAlerts = (AWS) => {
-  const alert = {
-    "type": "security",
-    "lon": -115.1697,
-    "lat": 36.1212,
-    "description": "this is sparta",
-    "time": "2017-11-26T10:34:56.123Z"
-  }
-
-  const alert2 = {
-    "type": "security",
-    "lon": -115.1597,
-    "lat": 36.1292,
-    "description": "this is las vegas",
-    "time": "2017-11-26T10:34:56.123Z"
-  }
-
-  return Promise.resolve([ alert, alert2 ])
-
   let docClient = new AWS.DynamoDB.DocumentClient();
   const params = {
     TableName: window.config.aws.dynamoPendingAlerts,
-    KeyConditionExpression: "#s = 'pending'",
+    /*
+    TODO should do some filtering
+
+    FilterExpression: '#s = :pending',
     ExpressionAttributeNames: {
-      "#s": "status"
+      '#s': 'status'
     },
+    ExpressionAttributeValues: {
+      ':pending': {S: 'pending'}
+    },
+    */
   }
   return new Promise((resolve, reject) => {
-    docClient.query(params, (err, data) => {
+    docClient.scan(params, (err, data) => {
       if(err) {
         console.error('Failed to fetch pending alerts', err)
         reject(err)
+        return
       }
-      resolve(data)
+      // TODO should filter in database
+      resolve(_.filter(data.Items, item => item.status !== 'pending'))
     })
   })
 }

c71528298a3f19d77375dd1691f2437a62ccaccc
mlison <michal.lison@nitor.fi>
2017-11-28 03:29:05 +0200

    pan to map

--- admin-ui/src/map-display/map-display.js
+++ admin-ui/src/map-display/map-display.js
@@ -6,10 +6,13 @@ import {
   Marker,
 } from "react-google-maps";
 // { lat: -34.397, lng: 150.644 }
-const MapDisplay = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
+const MapDisplay = withScriptjs(withGoogleMap(props => {
+  const { lat, lng } = props.positions && props.positions[0]
+
+  return (<GoogleMap
     defaultZoom={10}
     defaultCenter={props.positions[0]}
+    ref={(map) => map && map.panTo({ lat, lng })}
   >
     {props.positions.map(pos => (
       <Marker key={JSON.stringify(pos)}
@@ -17,7 +20,7 @@ const MapDisplay = withScriptjs(withGoogleMap(props =>
       />
 
     ))}
-  </GoogleMap>
-))
+  </GoogleMap>)
+}))
 
 export default MapDisplay
